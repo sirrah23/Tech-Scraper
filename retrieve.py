68869e02a53a518fbc444f879d2f7c15c3790dbf
sirrah23 <hseth93@gmail.com>
2017-03-16 00:41:14 -0400

    Can get data from first two pages of Reddit programming

--- retrieve.py
+++ retrieve.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
 #TODO: This could totally be a generator
@@ -26,9 +26,33 @@ class HNParser(object):
 			content.append({'link': link, 'title': title})
 		return content 
 
+class REDParser(object):
+
+	def __init__(self):
+		self.base_link = 'https://www.reddit.com/r/programming.json?count='
+		self.page_limit = 2
+		self.page_size = 25
+		self.text = ''
+
+	def get_page(self):
+		link = self.base_link + str(self.page_limit * self.page_size)
+		#TODO: Check status code
+		r = requests.get(link)
+		self.text = r.text
+
+	def content_arr(self):
+		page_content = json.loads(self.text)['data']['children']
+		build_list = lambda acc, item: acc + [{'link': item['data']['url'], 'title': item['data']['title']}]
+		content = reduce(build_list, page_content, []) 
+		return content
+		
+		
 # Quick Test
-hn = HNParser()
-hn.get_page(1)
-print hn.content_arr()
-hn.get_page(2)
-print hn.content_arr()
+# hn = HNParser()
+# hn.get_page(1)
+# print hn.content_arr()
+# hn.get_page(2)
+# print hn.content_arr()
+rd = REDParser()
+rd.get_page()
+print rd.content_arr()
